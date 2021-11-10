01a2bf7d3061063b61285d3c2ca25f73464b219a
Luis <luisgomez@unisangil.edu.co>
2021-11-09 19:38:38 -0500

    feat: :sparkles: Add configuration talwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      backgroundImage: {
+        'lines-texture': "url('/css/img/pattern.png')"
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
