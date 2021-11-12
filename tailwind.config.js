9cf2802fc5b7c4fb30fdd3173244783e1db72009
Luis <luisgomez@unisangil.edu.co>
2021-11-12 13:14:09 -0500

    feat: :sparkles: Animations and variants

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,11 +9,29 @@ module.exports = {
       colors: {
         'azul-claro': "#37bcf9",
         'azul-oscuro': "#0370b9"
+      },
+      animation: {
+        'spin-slow': 'spin 2s linear infinite',
+        'from-below': 'from-below 500ms linear',
+        'from-right': 'from-right 300ms linear'
+      },
+      keyframes: {
+        'from-below': {
+          '0%': { transform: 'translateY(0%)' },
+          '50%': { transform: 'translateY(200%)'},
+          '100%': { transform: 'translateY(0%)' }
+        },
+        'from-right': {
+          '0%': { transform: 'translateX(200%)' },
+          '100%': { transform: 'translateX(0%)' }
+        }
       }
     },
   },
   variants: {
-    extend: {},
+    extend: {
+      animation: ['group-hover', 'hover']
+    },
   },
   plugins: [],
 }
